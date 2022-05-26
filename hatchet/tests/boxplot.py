1dd37cc9e347bde256a5a946425f0ea1115ef749
jarusified <jarus3001@gmail.com>
2022-05-26 14:45:26 -0400

    Fix headers

--- hatchet/tests/boxplot.py
+++ hatchet/tests/boxplot.py
@@ -1,7 +1,8 @@
-# Copyright 2017-2021 Lawrence Livermore National Security, LLC and other
+# Copyright 2017-2022 Lawrence Livermore National Security, LLC and other
 # Hatchet Project Developers. See the top-level LICENSE file for details.
 #
 # SPDX-License-Identifier: MIT
+
 import pandas as pd
 
 import hatchet as ht
