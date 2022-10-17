4063198bed99498c73f40d155da75a4026072fcc
Stephanie Labasan Brink <brink2@llnl.gov>
2022-10-17 13:18:56 -0700

    bump for 2022.2.1 minor release (#67)
    - update CHANGELOG
    - bump version number

--- docs/conf.py
+++ docs/conf.py
@@ -29,9 +29,9 @@ copyright = "2017-2022, Lawrence Livermore National Security, LLC"
 author = "LLNL Developerss"
 
 # The short X.Y version
-version = "2022.2.0"
+version = "2022.2.1"
 # The full version, including alpha/beta/rc tags
-release = "2022.2.0"
+release = "2022.2.1"
 
 
 # -- General configuration ---------------------------------------------------
