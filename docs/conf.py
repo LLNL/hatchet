a71e8ece0543cad6ae539e512bfec164778e656f
Stephanie Brink <brink2@llnl.gov>
2022-10-24 10:50:22 -0700

    bump for 2022.2.2 hotfix release

--- docs/conf.py
+++ docs/conf.py
@@ -29,9 +29,9 @@ copyright = "2017-2022, Lawrence Livermore National Security, LLC"
 author = "LLNL Developerss"
 
 # The short X.Y version
-version = "2022.2.1"
+version = "2022.2.2"
 # The full version, including alpha/beta/rc tags
-release = "2022.2.1"
+release = "2022.2.2"
 
 
 # -- General configuration ---------------------------------------------------
