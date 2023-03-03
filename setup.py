2e624c8497c6adacd1c5010f4301da2ed6276233
ilumsden <lumsden.ian@gmail.com>
2023-03-03 16:36:24 -0500

    Updates build/dist code (i.e., setup.py) to account for removal of Cython code

--- setup.py
+++ setup.py
@@ -5,8 +5,10 @@
 
 from setuptools import setup
 from setuptools import Extension
+from setuptools.command.build_ext import build_ext as build_ext_orig
 from codecs import open
 from os import path
+import sys
 
 here = path.abspath(path.dirname(__file__))
 
@@ -19,6 +21,42 @@ with open(path.join(here, "README.md"), encoding="utf-8") as f:
 version = {}
 with open("./hatchet/version.py") as fp:
     exec(fp.read(), version)
+
+
+# Obtained from https://github.com/himbeles/ctypes-example/blob/master/setup.py
+class CTypesExtension(Extension):
+    pass
+
+
+class build_ext(build_ext_orig):
+
+    def __init__(self, *args, **kwargs):
+        self._ctypes = False
+        if sys.version_info[0] == 2:
+            super(build_ext, self).__init__(*args, **kwargs)
+        else:
+            super().__init__(*args, **kwargs)
+
+    def build_extension(self, ext):
+        self._ctypes = isinstance(ext, CTypesExtension)
+        if sys.version_info[0] == 2:
+            return super(build_ext, self).build_extension(ext)
+        else:
+            return super().build_extension(ext)
+
+    def get_export_symbols(self, ext):
+        if self._ctypes:
+            return ext.export_symbols
+        if sys.version_info[0] == 2:
+            return super(build_ext, self).get_export_symbols(ext)
+        return super().get_export_symbols(ext)
+
+    def get_ext_filename(self, ext_name):
+        if self._ctypes:
+            return ext_name + ".so"
+        if sys.version_info[0] == 2:
+            return super(build_ext, self).get_ext_filename(ext_name)
+        return super().get_ext_filename(ext_name)
 
 
 setup(
@@ -41,7 +79,7 @@ setup(
         "hatchet.util",
         "hatchet.external",
         "hatchet.tests",
-        "hatchet.cython_modules.libs",
+        "hatchet.extension",
     ],
     install_requires=[
         "pydot",
@@ -55,13 +93,18 @@ setup(
         "caliper-reader",
     ],
     ext_modules=[
-        Extension(
-            "hatchet.cython_modules.libs.reader_modules",
-            ["hatchet/cython_modules/reader_modules.c"],
+        CTypesExtension(
+            "hatchet.extension.libreader_modules",
+            [
+                "hatchet/extension/reader_modules.c",
+            ]
         ),
-        Extension(
-            "hatchet.cython_modules.libs.graphframe_modules",
-            ["hatchet/cython_modules/graphframe_modules.c"],
+        CTypesExtension(
+            "hatchet.extension.libgraphframe_modules",
+            [
+                "hatchet/extension/graphframe_modules.c",
+            ],
         ),
     ],
+    cmdclass={"build_ext": build_ext},
 )
