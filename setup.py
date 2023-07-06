da5bd4518c64c6588929141a57713039296d7d9a
Dewi Yokelson <dewi.yokelson@gmail.com>
2023-07-06 09:42:39 -0700

    removing python2 references as no longer supported

--- setup.py
+++ setup.py
@@ -94,7 +94,7 @@ setup(
         "License :: OSI Approved :: MIT License",
     ],
     keywords="",
-    python_requires=">=2.7,!=3.0.*,!=3.1.*,!=3.2.*,!=3.3.*,!=3.4.*",
+    python_requires=">=3.5",
     packages=[
         "hatchet",
         "hatchet.readers",
