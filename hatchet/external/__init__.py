452474f4bcdaf40d478eab7a33d1ac34f73bcaa2
Connor Scully-Allison <cscullyallison@sci.utah.edu>
2024-03-11 16:25:34 -0600

    Added Jupyter Version Check to Roundtrip Import (#116)
    * Added a version check to location of roundtrip import. Will throw warning if roundtrip fails to load.
    
    * Fixed formatting issues identified by flake. Black configuration seems to have erros
    
    * black fixes
    
    ---------
    
    Co-authored-by: Stephanie Brink <brink2@llnl.gov>

--- hatchet/external/__init__.py
+++ hatchet/external/__init__.py
@@ -2,9 +2,32 @@
 # Hatchet Project Developers. See the top-level LICENSE file for details.
 #
 # SPDX-License-Identifier: MIT
+
+
+class VersionError(Exception):
+    """
+    Define a version error class with the same features of the base exception class.
+    Allows us to catch the very specific ipython version error and throw a warning.
+    """
+
+    pass
+
+
 try:
     from .roundtrip.roundtrip.manager import Roundtrip
+    import IPython
 
+    # Refrencing Roundtrip here to resolve scope issues with import
     Roundtrip
+
+    # Testing IPython version
+    if int(IPython.__version__.split(".")[0]) > 7:
+        raise VersionError()
+
 except ImportError:
     pass
+
+except VersionError:
+    print(
+        "Warning: Roundtrip module could not be loaded. Requires jupyter notebook version <= 7.x."
+    )
