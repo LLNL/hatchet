83482ba5e59757f511d7eea0fa64c2c9cf69e3c4
Stephanie Brink <brink2@llnl.gov>
2024-04-11 10:30:13 -0700

    cleaning up code

--- hatchet/graphframe.py
+++ hatchet/graphframe.py
@@ -447,11 +447,11 @@ class GraphFrame:
             raise ve
 
     @staticmethod
-    def from_perfflowaspect(filename, **kwargs):
+    def from_perfflowaspect(filename):
         # import this lazily to avoid circular dependencies
         from .readers.perfflowaspect_reader import PerfFlowAspectReader
 
-        return PerfFlowAspectReader(filename).read(**kwargs)
+        return PerfFlowAspectReader(filename).read()
 
     @deprecated(
         "Reading from/writing to HDF5 is deprecated and will be removed in a later version."
