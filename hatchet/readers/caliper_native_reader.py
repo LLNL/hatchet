f4cb8d11a9677503b185ee37b1af2ad12a8e6bbd
Michael McKinsey <michaelmckinsey1@gmail.com>
2025-04-08 11:37:43 -0700

    Aggregate on nid only if timeseries

--- hatchet/readers/caliper_native_reader.py
+++ hatchet/readers/caliper_native_reader.py
@@ -87,7 +87,11 @@ class CaliperNativeReader:
             if self.filename_or_caliperreader.attribute(col).is_value():
                 self.metric_cols.append(col)
         df_metrics = pd.DataFrame.from_dict(data=metrics)
-        df_new = df_metrics.groupby(df_metrics["nid"]).aggregate("first").reset_index()
+        # Aggregate on nid if timeseries data
+        if "timeseries.snapshot" in df_metrics:
+            df_new = df_metrics.groupby(df_metrics["nid"]).aggregate("first").reset_index()
+        else:
+            df_new = df_metrics
         return df_new
 
     def _reset_metrics(self, metrics):
