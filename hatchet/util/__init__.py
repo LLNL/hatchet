2c23ad2183e89d187c9073cd85f9f2c23338e904
Ian Lumsden <lumsden.ian@gmail.com>
2025-06-06 14:04:40 -0700

    Allows Hatchet to work with NumPy >= 2.0 (#163)
    * Allows Hatchet to work with NumPy >= 2.0
    
    * Adds various warnings and errors to inform users about complications with to_hdf/from_hdf
    
    * Tweaks things to skip tests as needed for HDF
    
    * Small logic fix
    
    * Formatting
    
    * Allows use of 'deprecated' macro for earlier Python versions
    
    * Formatting
    
    * Linting
    
    * Corrects decorator order

--- hatchet/util/__init__.py
+++ hatchet/util/__init__.py
@@ -2,3 +2,13 @@
 # Hatchet Project Developers. See the top-level LICENSE file for details.
 #
 # SPDX-License-Identifier: MIT
+
+import sys
+
+import numpy as np
+
+
+def _validate_numpy_version_for_hdf():
+    is_py_lt_3_10 = sys.version_info < (3, 10)
+    is_np_ge_2 = tuple(map(int, (np.__version__.split(".")))) >= (2, 0)
+    return is_py_lt_3_10 and is_np_ge_2
