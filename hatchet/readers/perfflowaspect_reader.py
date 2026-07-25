19adda9ec3b880e75e1192199c2829cca9d99b97
Stephanie Brink <brink2@llnl.gov>
2026-07-24 23:26:15 -0700

    fix license issues

--- hatchet/readers/perfflowaspect_reader.py
+++ hatchet/readers/perfflowaspect_reader.py
@@ -1,3 +1,9 @@
+# Copyright 2017-2023 Lawrence Livermore National Security, LLC and other
+# Hatchet Project Developers. See the top-level LICENSE file for details.
+#
+# SPDX-License-Identifier: MIT
+
+
 import json
 import pandas as pd
 import re
